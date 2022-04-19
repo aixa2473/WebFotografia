4765860fd8cc85a5ab57aa62be7a02a66083e907
aixa2473 <aixa.ratta@gmail.com>
2022-04-19 00:07:41 -0300

    Proyecto final

--- basededatos.sql
+++ basededatos.sql
@@ -1,1 +1,16 @@
-SELECT * FROM trabajofinal.conta;
+create database trabajofinal;
+use trabajofinal;
+create table conta(
+conta_id int unsigned not null auto_increment,
+conta_nombre varchar(150),
+conta_correo varchar(150),
+conta_telefono int not null,
+conta_descripcion varchar(150),
+primary key(conta_id)
+)
+
+
+
+
+
+
