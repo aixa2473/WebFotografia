773c0526b95431e0dc0d5bb1b17ddc937bf379d6
aixa2473 <aixa.ratta@gmail.com>
2022-04-18 23:43:56 -0300

    Proyecto final

--- index.js
+++ index.js
@@ -9,127 +9,54 @@ require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 //Conectamos a la Base de Datos
-/*const conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "berni",
-    database: "fullstack"
-}); */
+    password: "papelitos1",
+    database: "trabajofinal"
+});
 
-/*conexion.connect((error) =>{
+conexion.connect((error) =>{
     if(error) throw error;
     console.log('Conexión a la Base de Datos correcta!!');
-}); */
+});
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/assets", express.static(__dirname + "/public"));
+app.use('/public',express.static('Public'));
+app.use('/multimedia',express.static('multimedia'));
 
 //Configuramos el Motor de Plantillas
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
-app.use(express.static("./static"));
+
 
 app.get('/', (req, res) =>{
-    res.render('index', {titulo: 'Bienvenidos a la App'})
+    res.render('index')
 });
 
 app.get('/quienessomos', (req, res) =>{
-    res.render('quienessomos', {titulo: 'Quienes Somos'})
+    res.render('quienessomos')
 });
 
 app.get('/productos', (req, res) =>{
-    res.render('productos', {titulo: 'Productos'})
-});
-
-app.get('/formulario', (req, res) =>{
-    res.render('formulario', {titulo: 'Formulario para Completar'})
-});
-
-
-app.post('/formulario', (req, res) =>{
-    /*  res.json({
-        Dato: 'Recibido'
-    }); */
-
-    //Desestructuración de las variables
-    const { nombre, precio, descripcion } = req.body;
-        
-    if(nombre == "" || precio == ""){
-        
-        let validacion = 'Faltan datos para guardar el Producto';
-        
-        res.render('formulario', {
-            titulo: 'Formulario para Completar',
-            validacion
-        });
-
-    }else{
-
-        let data = {
-            producto_nombre: nombre,
-            producto_precio: precio,
-        };
-        
-        let sql = "INSERT INTO productos SET ?";
-
-        let query = conexion.query(sql, data, (err, results) => {
-            if (err) throw err;
-            res.render('formulario', {titulo: 'Formulario para Completar'});
-        }); 
-    }
-});
-
-app.get('/productos', (req, res) =>{
-    let sql = " SELECT * FROM productos";
-    let query = conexion.query(sql, (err, results) => {
-        if (err) throw err;
-        res.render("productos", {
-            titulo: 'Productos',
-            results: results,
-        });
-        });
-});
-
-//UPDATE
-app.post("/update", (req, res) => {
-    let sql =
-    "UPDATE productos SET producto_nombre='" +
-    req.body.producto_nombre +
-    "', producto_precio='" +
-    req.body.producto_precio +
-    "' WHERE producto_id=" +
-    req.body.id;
-    let query = conexion.query(sql, (err, results) => {
-    if (err) throw err;
-    res.redirect("/");
-    });
-});
-
-
-// DELETE
-
-app.post("/delete", (req, res) => {
-    let sql = "DELETE FROM productos WHERE producto_id=" + req.body.producto_id + "";
-    let query = conexion.query(sql, (err, results) => {
-        if (err) throw err;
-        res.redirect("/");
-    });
+    res.render('productos')
 });
 
 app.get('/contacto', (req, res) =>{
-    res.render('contacto', {titulo: 'Escríbenos'})
+    res.render('contacto')
 });
 
 app.post('/contacto', (req, res) =>{
 
     //Desestructuración de las variables
-    const { nombre, email } = req.body;
+    const { nombre, correo, telefono, descripcion } = req.body;
         
-    if(nombre == "" || email == ""){
+    if(nombre == "" || telefono == "" || descripcion == "" ){
         
         let validacion = 'Faltan tus datos';
         
@@ -139,44 +66,26 @@ app.post('/contacto', (req, res) =>{
         });
 
     }else{
-        console.log(nombre);
-        console.log(email);
+        //INSERTAR datos a DB
 
-        async function main(){
 
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 465,
-                secure: true,
-                auth: {
-                    user: "Tu Correo Electrónico / Empresa",
-                    pass: "xxxxxxxxxxxxxxxx",
-                }
-            });
+        let data = {
+            conta_nombre: nombre,
+            conta_correo: correo,
+            conta_telefono: telefono,
+            conta_descripcion: descripcion,
+        };
+        
+        let sql = "INSERT INTO conta SET ?";
 
-            let info = await transporter.sendMail({
-                from: 'Tu Correo Electrónico / Empresa',
-                to: `${email}`,
-                subject: "Nuevo mensaje de Contacto",
-                html: `Gracias por contactar con nosotros <br>
-                Nuestras promociones las puedes encontrar en nuestro <br>
-                sitio web .....heroku.....
-                `
-            });
-
-            res.render('enviado', {
-                titulo: 'Mail Enviado',
-                nombre,
-                email
-            }); 
-        }
-
-        main().catch(console.error);
-
+        conexion.query(sql, data, (err, results) => {
+            if (err) throw err;
+            res.render('contacto');
+        }); 
     }
-
-
 });
+
+
 
 
 app.listen(PORT, () =>{
